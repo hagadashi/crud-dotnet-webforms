f393ffe0ad4e1d425d691343a2f5f6f6aa382a2e
hagadashi <37146007+hagadashi@users.noreply.github.com>
2018-11-16 12:52:35 -0200

    initial commit with gitignore

---
+++ TarefasAcademicas/Repositorys/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace Repositorys
+{
+    public abstract class Repository
+    {
+        protected OleDbConnection dbConnection;
+
+        protected Repository()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["conexaoAccess"].ToString();
+            dbConnection = new OleDbConnection(connectionString);
+        }
+
+        protected OleDbCommand CmdFactory(string query)
+        {
+            OleDbCommand cmd = new OleDbCommand(query, dbConnection);
+
+            return cmd;
+        }
+    }
+}
---
+++ TarefasAcademicas/Repositorys/Tarefa.Repository.cs
@@ -0,0 +1,57 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Repositorys
+{
+    public class TarefaRepository : Repository
+    {
+        private readonly OleDbParameter pDataEntrega = new OleDbParameter("@DataEntrega", OleDbType.Date);
+        private readonly OleDbParameter pDescricao = new OleDbParameter("@Descricao", OleDbType.VarChar);
+        private readonly OleDbParameter pEntregue = new OleDbParameter("@Entregue", OleDbType.Boolean);
+        private readonly OleDbParameter pId = new OleDbParameter("@Id", OleDbType.BigInt);
+        private readonly OleDbParameter pNota = new OleDbParameter("@Nota", OleDbType.TinyInt);
+        private readonly OleDbParameter pTipo = new OleDbParameter("@Tipo", OleDbType.VarChar);
+        private readonly OleDbParameter pTitulo = new OleDbParameter("@Titulo", OleDbType.VarChar);
+
+        public TarefaRepository(Tarefa tarefa) : base()
+        {
+            pDataEntrega.Value = tarefa.DataEntrega;
+            pDescricao.Value = tarefa.Descricao;
+            pEntregue.Value = tarefa.Entregue;
+            pId.Value = tarefa.Id;
+            pNota.Value = tarefa.Nota;
+            pTipo.Value = tarefa.Tipo;
+            pTitulo.Value = tarefa.Titulo;
+        }
+
+        public bool Cadastrar()
+        {
+            dbConnection.Open();
+
+            string query = $"INSERT INTO tbTarefas (titulo, descricao, dataEntrega, tipo) " +
+                $"VALUES ({pTitulo.ParameterName}, {pDescricao.ParameterName}, {pDataEntrega.ParameterName}, {pTipo.ParameterName})";
+
+            var cmd = CmdFactory(query);
+
+            cmd.Parameters.Add(pTitulo);
+            cmd.Parameters.Add(pDescricao);
+            cmd.Parameters.Add(pDataEntrega);
+            cmd.Parameters.Add(pTipo);
+
+            try
+            {
+                return (cmd.ExecuteNonQuery() > 0);
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+    }
+}
