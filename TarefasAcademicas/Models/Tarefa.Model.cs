4e1c042bcb6f01b0b20d7d12f92a6a9b94d4822f
hagadashi <37146007+hagadashi@users.noreply.github.com>
2018-11-16 12:37:14 -0200

    initial commit

---
+++ TarefasAcademicas/Models/Tarefa.Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Tarefa
+    {
+        public DateTime DataEntrega { get; set; }
+        public string Descricao { get; set; }
+        public bool Entregue { get; set; }
+        public int Id { get; private set; }
+        public byte Nota { get; set; }
+        public string Tipo { get; set; }
+        public string Titulo { get; set; }
+    }
+}
