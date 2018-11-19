2c3b8ba8999672d9bb3cd678ac032dbbf4ac84dc
hagadashi <37146007+hagadashi@users.noreply.github.com>
2018-11-18 22:47:43 -0200

    correção do merge

--- TarefasAcademicas/TarefasAcademicas/Cadastro.aspx.cs
+++ TarefasAcademicas/TarefasAcademicas/Cadastro.aspx.cs
@@ -46,13 +46,9 @@ namespace TarefasAcademicas
             }
         }
 
-<<<<<<< HEAD
         protected void btnCancelar_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/");
         }
-=======
-        
->>>>>>> cfbd535ff75001c44a4b71f1fb3b4f95d3def9e1
     }
 }
